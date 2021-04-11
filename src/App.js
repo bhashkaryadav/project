d0df93a423f4e7b30dd964981496509654634124
bhashkaryadav <yadavbhasker6210@gmail.com>
2021-04-11 06:57:15 +0530

    created three buttons

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
     <Router >
     <Switch>
       <Route path="/" component={Home} exact />
-    
+      <Route path="/signin" component={SigninPage} exact />
+     
       
     </Switch>
     </Router>
